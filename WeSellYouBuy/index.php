4a25500e7ed3f08cce06f113013f1f4b1093b2b9
Aldo1540 <36041281+Aldo1540@users.noreply.github.com>
2018-11-10 15:45:36 -0600

    WeSellYouBuy
    includes index.php, images folder, includes folder, and style folder

---
+++ WeSellYouBuy/index.php
@@ -0,0 +1,30 @@
+<?php include 'includes/signedOutHeader.php'; ?>
+
+	<div class="advertisments">
+		
+		<img src="images/placeholder.png" class="center">
+	</div>
+	<div class="content">
+		<h1>Content goes here</h1>
+		<ul class="contentImgs">
+			<li><img src="images/content.png"></li>
+			<li><img src="images/content.png"></li>
+			<li><img src="images/content.png"></li>
+			<li><img src="images/content.png"></li>
+			<li><img src="images/content.png"></li>
+		</ul>
+		<ul class="contentImgs">
+			<li><img src="images/content.png"></li>
+			<li><img src="images/content.png"></li>
+			<li><img src="images/content.png"></li>
+			<li><img src="images/content.png"></li>
+			<li><img src="images/content.png"></li>
+		</ul>	
+	</div>
+
+	<footer>
+	<p>Footer</p>
+	</footer>
+
+</body>
+</html>
