89a14f735d0dcc2999db25642e5a028eb2bf1984
Aldo Ramirez <agw565@my.utsa.edu>
2018-11-11 11:53:13 -0600

    Added different pages, still not designed

---
+++ WeSellYouBuy/cart.php
@@ -0,0 +1,16 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<title>We Sell You Buy</title>
+	<link rel="stylesheet" type="text/css" href="style/core.css">
+</head>
+<body>
+	<?php include 'includes/signedOutHeader.php'; ?>
+	<h1>CART<h1>
+	
+	<footer>
+	<p>Footer</p>
+	</footer>
+	
+</body>
+</html>
