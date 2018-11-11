221b5433898632e8b2d946017665272f3b30c7a2
Aldo Ramirez <agw565@my.utsa.edu>
2018-11-10 23:03:38 -0600

    Testing git commit with git bash

--- WeSellYouBuy/index.php
+++ WeSellYouBuy/index.php
@@ -5,7 +5,6 @@
 		<img src="images/placeholder.png" class="center">
 	</div>
 	<div class="content">
-		<h1>Content goes here</h1>
 		<ul class="contentImgs">
 			<li><img src="images/content.png"></li>
 			<li><img src="images/content.png"></li>
