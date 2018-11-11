b10efb3e7efa213cd022ba2e7ecaca4ee758ee80
Aldo Ramirez <agw565@my.utsa.edu>
2018-11-11 11:58:55 -0600

    Different pages include seperate .css sheets

--- WeSellYouBuy/itempage.php
+++ WeSellYouBuy/itempage.php
@@ -3,6 +3,7 @@
 <head>
 	<title>We Sell You Buy</title>
 	<link rel="stylesheet" type="text/css" href="style/core.css">
+	<link rel="stylesheet" type="text/css" href="style/itempage.css">
 </head>
 <body>
 	<?php include 'includes/signedOutHeader.php'; ?>
