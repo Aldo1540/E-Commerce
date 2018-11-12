a5efa0e27c49e3f6bd981067310db91409527a2e
Aldo Ramirez <agw565@my.utsa.edu>
2018-11-12 11:36:06 -0600

    Styled the login page

--- WeSellYouBuy/signin.php
+++ WeSellYouBuy/signin.php
@@ -8,13 +8,13 @@
 <body>
 	<?php include 'includes/signedOutHeader.php'; ?>
 	<div class="logIn">
-											<h1>Sign in </h1>
-											<form method="post" action="includes/login.inc.php">
-													<input type="text" name="Username" placeholder="Username"> <br />
-													<input type="password" name="Password" placeholder="Password"><br />
-
-													<input type="submit" name="login-submit" value="Submit">
-											</form>
+	<img src="images/logo2.png" style="width:10%;">
+	<h1>Sign in</h1>
+		<form method="post" action="includes/login.inc.php">
+			<input type="text" name="Username" placeholder="Username"><br>
+			<input type="password" name="Password" placeholder="Password"><br>
+			<input class="logInbtn" type="submit" name="login-submit" value="Submit">
+		</form>
 	</div>
 	<!-- log in form in html to take a username and a password,
 			 checks for $_SERVER["PHP _SELF"] expoits on data entry,
