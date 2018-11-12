21999c65081907c9d5392042391ab231dc140913
Aldo Ramirez <agw565@my.utsa.edu>
2018-11-11 18:24:43 -0600

    Added random images

--- WeSellYouBuy/index.php
+++ WeSellYouBuy/index.php
@@ -9,22 +9,22 @@
 	<?php include 'includes/signedOutHeader.php'; ?>
 	
 	<div class="advertisments">
-		<img src="images/placeholder.png" class="center">
+		<img src="images/sale/add.png" class="center">
 	</div>
 	<div class="content">
 		<ul class="contentImgs">
-			<li><img src="images/content.png"></li>
-			<li><img src="images/content.png"></li>
-			<li><img src="images/content.png"></li>
-			<li><img src="images/content.png"></li>
-			<li><img src="images/content.png"></li>
+			<li><img src="images/items/chair.png"></li>
+			<li><img src="images/items/grill.png"></li>
+			<li><img src="images/items/speaker.png"></li>
+			<li><img src="images/items/table.png"></li>
+			<li><img src="images/items/multitool.png"></li>
 		</ul>
 		<ul class="contentImgs">
-			<li><img src="images/content.png"></li>
-			<li><img src="images/content.png"></li>
-			<li><img src="images/content.png"></li>
-			<li><img src="images/content.png"></li>
-			<li><img src="images/content.png"></li>
+			<li><img src="images/items/towels.png"></li>
+			<li><img src="images/items/shirt.png"></li>
+			<li><img src="images/items/blender.png"></li>
+			<li><img src="images/items/babybed.png"></li>
+			<li><img src="images/items/lawnmower.png"></li>
 		</ul>	
 	</div>
 
