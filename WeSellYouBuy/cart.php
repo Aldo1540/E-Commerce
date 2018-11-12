78a4a4b3c9b9150c1ae657ed5238213295e04a07
alberto20 <adalbertoramirez19@yahoo.com>
2018-11-11 19:11:35 -0600

    Add files via upload
    Added table to product, price, quanity and image of product

--- WeSellYouBuy/cart.php
+++ WeSellYouBuy/cart.php
@@ -5,13 +5,48 @@
 	<link rel="stylesheet" type="text/css" href="style/core.css">
 	<link rel="stylesheet" type="text/css" href="style/cart.css">
 </head>
-<body>
-	<?php include 'includes/signedOutHeader.php'; ?>
-	<h1>CART<h1>
+	<?php include 'includes/signedOutHeader.php'; 
+
+?>
+	<h1>WeSellYouBuy<h1>
+	<header id="title">
+      <h1>My Shopping Cart</h1>
+	  
+     
+	 <body>
+	 <div id="wrap">
+	<table style="width:40%">
+	<tr>
+		<th>Image</th>
+		<th>Product</th>
+		<th>Quantity</th>
+		<th>Price</th>
+	</tr>
+	<tr>
+		<td><li><img src="images/items/blender.png" style="width:30%"; "height:30%";></li><td>
+		<td>Blender</td>
+		<td>1</td>
+		<td>19.99</td>
+	</tr>
+	<tr>
+		<td><li><img src="images/items/shirt.png" style="width:30%"; "height:30%";></li><td>
+		<td>Shirt</td>
+		<td>1</td>
+		<td>9.99</td>
+	</tr>
+	<tr>
+		<td><li><img src="images/items/grill.png" style="width:30%"; "height:30%";></li><td>
+		<td>grill</td>
+		<td>1</td>
+		<td>149.99</td>
+	</tr>
+	</table> 
+	</body>
 	
-	<footer>
-	<p>Footer</p>
-	</footer>
-	
-</body>
+	<body>
+	<button type="button">Remove</button> 
+	<button type="button">Remove All</button> 
+	<button type="button">Purchase</button> 
+	<label> Price: $179.97</label>
+
 </html>
